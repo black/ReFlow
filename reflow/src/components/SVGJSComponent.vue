966a6e594525344573c0379d0956ff7d08002fa2
Λ B H I N Λ V <ixd.abhi@gmail.com>
2022-07-03 18:43:41 +0530

    move attributes

--- reflow/src/components/SVGJSComponent.vue
+++ reflow/src/components/SVGJSComponent.vue
@@ -10,23 +10,20 @@ export default {
     data() {
         return { 
             size: 600, 
-            svgContainer: null
+            attributes:{
+                'fill': '#f06',
+                'stroke': "grey",
+                'stroke-width': 2
+            } 
         };
     },
     methods: {
-        drawPath() {
-            var attributes = {
-                fill: '#f06',
-                stroke: {
-                    color: "grey",
-                    width: 5
-                }
-            }
+        drawPath() { 
             //creating canvas and starting to creating the geometry   
             var canvas = this.svg().addTo(this.$refs.mysvg).size('100%', '100%')
             console.log(canvas)
             this.svgContainer = canvas;
-            canvas.rect(100, 100).attr(attributes)  
+            canvas.rect(100, 100).attr(this.attributes)  
         }
     },
     mounted() { 
