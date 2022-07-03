056bd71e692ee05eb5dad42f96722ed3ffd03500
Λ B H I N Λ V <ixd.abhi@gmail.com>
2022-07-03 12:57:00 +0530

    finally fixed the SVG JS load

--- reflow/src/main.js
+++ reflow/src/main.js
@@ -1,8 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'material-icons/iconfont/material-icons.css'
+import {
+    SVG
+} from '@svgdotjs/svg.js'
 import './index.css' 
- 
-createApp(App).mount('#app') 
 
- 
+
+const app = createApp(App)
+app.config.globalProperties.svg = SVG 
+app.mount('#app')
+
+
+
+/*  Bugs & Fixes -----------
+https: //stackoverflow.com/questions/65184107/how-to-use-vue-prototype-or-global-variable-in-vue-3
+provide/ syntax replaced Vue.prototype in Vue3
+
+GOJS = "https://gojs.net/latest/index.html?gclid=CjwKCAjw2f-VBhAsEiwAO4lNeNT8OvEwsKhJT5RaNr-SeuDBmR6aI62rUQHBlhqwgWcLMtmCRNvs2RoCrQEQAvD_BwE"
+*/ 
--- reflow/src/vueSvgPlugin.js
+++
@@ -1,9 +0,0 @@
-import {
-    SVG
-} from '@svgdotjs/svg.js'
-
-export default {
-    install: Vue => {
-        Vue.prototype.$svg = SVG
-    }
-}
